93b153004144f485da046d0646f49703310b30ea
puldavid87 <puldavid87@gmail.com>
2023-09-26 14:44:41 +0200

    commint 1

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import data_exploration as de
-
+import data_preprocessing as dp
 dataset_path = "C:/Users/paur/Documents/Invernadero/Greenhouse_project/dataset"
 train_path = dataset_path+"/train"
 test_path = dataset_path+"/test"
+destination_path = "C:/Users/paur/Documents/Invernadero/"
+
 #first try
 de.autentication('paulrosero/tomato-leaf-illness-detection')
 de.unzipfile('tomato-leaf-illness-detection.zip')
@@ -14,10 +16,7 @@ de.shape_labels(labels,train_path)
 
 flatten_dataset = de.get_flatten_dataset(train_path,labels)
 
-tsne_data = de.tsne_method(flatten_dataset)
-
-pca_data = de.pca_method(flatten_dataset)
-
 new_labels = de.flatten_labels(labels,train_path)
 
-de.plot_dr_figure(tsne_data,new_labels )
+
+
