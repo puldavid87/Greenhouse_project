411fe87beddf7ce31c82af9e0cf02f6c28518655
puldavid87 <puldavid87@gmail.com>
2023-10-03 15:29:50 +0200

    final_round

--- tf_cnn.py
+++ tf_cnn.py
@@ -8,6 +8,7 @@ import Xception
 import ConvNext 
 import os
 import tensorflow as tf
+
 #Datasets
 dataset_path = "C:/Users/paur/Documents/Invernadero/Greenhouse_project/dataset"
 train_path = dataset_path + "/train"
@@ -18,12 +19,6 @@ os.makedirs(output_path, exist_ok=True)
 labels, tam_labels = de.get_labels(train_path)
 dak.rotation_per_image(train_path, labels, output_path)
 
-#Models
-folder_name = "Efficient"
-path_model_destination = "C:/Users/paur/Documents/Invernadero/Greenhouse_project/Models/" + folder_name + "/"
-path_data_source = "C:/Users/paur/Documents/Invernadero/Greenhouse_project/dataset"
-test_dir = "C:/Users/paur/Documents/Invernadero/Greenhouse_project/dataset/test"
-
 #variables
 classes = 4
 epochs = 20
@@ -32,20 +27,35 @@ img_height = 224
 img_width = 224
 # Define some parameters for the loader:
 batch_size = 32
-
-cnn_tf.make_folder(folder_name, path_model_destination)
-
-train_data, validation_data, test_data = cnn_tf.split_tratin_test_set(path_data_source,batch_size,img_height, img_width)
-
-efficient_model = Efficient.build_model(classes, False)
-inceptionV3 = Inception.build_model(classes, False)
-mobilenet_model = Mobilenet.build_model(classes, False)
-xception_model = Xception.build_model(classes, False)
-convnext_model =  ConvNext.build_model(classes, False)
-cnn_tf.unfreeze_model(efficient_model, 20) 
 callback = tf.keras.callbacks.EarlyStopping(monitor='val_accuracy', patience=5)
 
-cnn_tf.train_model(convnext_model, train_data, validation_data, test_data, callback, path_model_destination, 1,name="Inception_test1")
+models = ["Efficient", "Inception", "Mobilenet", "Xception", "Convext"]
+path_data_source = "C:/Users/paur/Documents/Invernadero/Greenhouse_project/dataset"
+test_dir = "C:/Users/paur/Documents/Invernadero/Greenhouse_project/dataset/test"
+train_data, validation_data, test_data = cnn_tf.split_tratin_test_set(path_data_source,batch_size,img_height, img_width)
 
+data_augmentation = [False, True]
+
+for augmentation in data_augmentation:
+    efficient_model = Efficient.build_model(classes, augmentation),
+    inceptionV3 = Inception.build_model(classes, augmentation),
+    mobilenet_model = Mobilenet.build_model(classes, augmentation),
+    xception_model = Xception.build_model(classes, augmentation),
+    convnext_model =  ConvNext.build_model(classes, augmentation) 
+    #Models
+    ml_models = [efficient_model,inceptionV3,mobilenet_model,xception_model,convnext_model]
+    for i, folder_name in enumerate (models):
+        path_model_destination = "C:/Users/paur/Documents/Invernadero/Greenhouse_project/Models/" + folder_name + "/"
+        cnn_tf.make_folder(folder_name, path_model_destination) 
+        cnn_tf.train_model(ml_models[i], train_data, validation_data, test_data, 
+                           callback, path_model_destination,epochs,name=str(folder_name) 
+                           + "_" + str(augmentation))
+        cnn_tf.unfreeze_model(ml_models[i], unfreeze_layers )
+        cnn_tf.train_model(ml_models[i], train_data, validation_data, test_data, 
+                           callback, path_model_destination,epochs,name=str(folder_name) 
+                           + "_" + str(augmentation) + "unfree")
+        
+        
+    
 
 
